e88f7db2d6fdc624cda36a518a5207155445b99d
Shan-Git-Coder <mrsabeshanoshan@gmail.com>
2025-06-28 05:10:32 -0700

    Boolean Condtion

---
+++ Conditions.java
@@ -0,0 +1,20 @@
+class Conditions{
+    public static void main(String[]args)
+    {
+        // if your age is under the 18 you can't Apply to this or you're over the 18 You can Apply to This
+
+        //Change the Number of Variable (age)
+        int age = 12;
+
+        //Condition
+        if(age>=18)
+        {
+            //Statement
+            System.out.print("You Can Apply");
+        }
+        else
+        {
+            System.out.print("You can't Apply");
+        }
+    }
+}
