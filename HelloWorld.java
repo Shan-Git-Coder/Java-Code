bcbc9c6a6836da2ed7b273a15235ad3ace941026
Shan Code <mrsabeshanoshan@gmail.com>
2025-06-30 19:15:51 +0530

    Addition 2 Numbers

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 class HelloWolrd{
     public static void main(String[]args)
     {
-        //Hello Wolrd
-        System.out.print("Hello World");
+        int num1 = 10;
+        int num2 = 10;
+        int num3 = num1+num2;
+        System.out.print(num3);
+
+        //Answer is 20
     }
 }
