ad67ec4002d6b8635b5aee71ae81b64867ed00df
Shan-Git-Coder <mrsabeshanoshan@gmail.com>
2025-06-30 12:09:38 +0530

    Username Check (String)

---
+++ Condition2.java
@@ -0,0 +1,15 @@
+class Condition2{
+    public static void main(String[]args)
+    {
+        //Username Check
+        String username = "Shan";
+
+        if(username=="Shan")
+        {
+                System.out.print("Username Correct");
+        }
+        else {
+            System.out.print("Invalid Username");
+        }
+    }
+}
